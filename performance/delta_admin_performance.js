7846cb6074c66bbb45ef818679663e5a94bdcb78
paulevy2 <paulevy@umail.iu.edu>
2015-02-02 16:28:21 -0800

    Added DSC performance demo and made separate map folder

---
+++ performance/delta_admin_performance.js
@@ -0,0 +1,98 @@
+var deltaplan_dataobject = {"groups": [
+	{ label: "Reliable Water", type: "Chapter", description: "<ul><li>24 Measures: 4 complete, 14 active, 6 pending </li><li> 5 agencies: DWR, USBOR, SWRCB, DSC, Water Commission </li><li> Linkage to: <ul><li> Water Management Plans </li><li> Bay Delta Conservation Plan </li><li> California Water Plan </li></ul></li></ul>", groups: [
+		{ label:"WR P1", type: "Policy", description: "Reduce Reliance on the Delta through Improved Regional Water Self-Reliance", gcolor: "#fff200", status: "Active", groups: [
+			{label: "WR P1-01", type: "Measure", description: "Identify number of water suppliers that have undertaken covered actions that have:  (1) completed a current urban or agricultural water management plan that has been reviewed by California Department of Water Resources for compliance with applicable legal requirements,  (2) commenced implementation of identified measures that will reduce reliance on the Sacramento-San Joaquin Delta, and  (3) starting in 2015, reported on the expected outcome for measureable reductions in reliance on the Sacramento-San Joaquin Delta and improvement in regional self-reliance as the reduction in the amount of water used, or the percentage of water used, from the Sacramento-San Joaquin Delta watershed.",
+			status: "Active", due_date: "1/1/2015"},
+			]},
+		{ label: "WR R01", type: "Recommendation", description: "Implement Water Efficiency and Water Management Planning Laws", status: "Active", groups: [
+			{label: "WR R01-01", type: "Measure", description: "Identify number of urban and agricultural water suppliers that certify they have adopted and are implementing supply planning, conservation, and efficiency measures required by State law by 2015, meeting the standards and deadlines established by code.",
+			status: "Active", due_date: "12/31/2015"},
+			]},
+		{ label: "WR R02", type: "Recommendation", description: "Require SWP Contractors to Implement Water Efficiency and Water Management Laws", status: "Active", groups: [
+			{label: "WR R02-01", type: "Measure", description: "California Department of Water Resources adopts and implements a requirement for State Water Project contracts and transfer agreements that requires implementation of State water efficiency, water management laws, goals, and regulations, including compliance with Water Code section 85021.",
+			status: "Pending", due_date: "NA"},
+			]},
+		{ label: "WR R03", type: "Recommendation", description: "Compliance with Reasonable and Beneficial Use", status: "Active", groups: [
+			{label: "WR R03-01", type: "Measure", description: "The State Water Resources Control Board adopts a policy that requires evaluation of new water rights or a new or changed point of diversion, place of use, or purpose that result in a new or increased long‐term average use of water from the Sacramento‐San Joaquin Delta watershed for consistency with reasonable and beneficial use, and Water Code sections 85021, 85023, and 85031, and other provisions of California law.",
+			status: "Active", due_date: "NA"},
+			]},
+		{ label: "WR R04", type: "Recommendation", description: "Expanded Water Supply Reliability Element", status: "Active", groups: [
+			{label: "WR R04-01", type: "Measure", description: "Identify percentage of urban and agricultural water suppliers that receive water from the Delta watershed that have incorporated an expanded Water Supply Reliability Element in their urban water management plan and agricultural water management plan by December 31, 2015.",
+			status: "Pending", due_date: "12/31/2015"},
+			]},
+		{ label: "WR R05", type: "Recommendation", description: "Develop Water Supply Reliability Element Guidelines", status: "Active", groups: [
+			{label: "WR R05-01", type: "Measure", description: "California Department of Water Resources has developed and published guidelines for the preparation of an expanded Water Supply Reliability Element by December 31, 2014.",
+			status: "Active", due_date: "12/31/2014"},
+			]},
+		{ label: "WR R06", type: "Recommendation", description: "Update Water Efficiency Goals", status: "Active", groups: [
+			{label: "WR R06-01", type: "Measure", description: "California Department of Water Resources and the State Water Resources Control Board have established an advisory group and identified impediments to achievement of statewide water conservation, recycled water, and storm water goals by 2014, and have evaluated and recommended update goals by 2018, including an assessment of how regions are achieving their proportional share of these goals.",
+			status: "Pending", due_date: "12/31/2014"},
+			]},
+		{ label: "WR R07", type: "Recommendation", description: "Revise State Grant and Loan Priorities", status: "Pending", groups: [
+			{label: "WR R07-01", type: "Measure", description: "State grant and loan ranking criteria have been revised by December 31, 2013.",
+			status: "Pending", due_date: "12/31/2013"},
+			]},
+		{ label: "WR R08", type: "Recommendation", description: "Demonstrate State Leadership", status: "Active", groups: [
+			{label: "WR R08-01", type: "Measure", description: "State agencies report to Delta Stewardship Council annually on their actions to demonstrate State leadership, to increase water efficiency, use recycled water, and incorporate storm water runoff capture and low‐impact development strategies.",
+			status: "Pending", due_date: "NA"},
+			]},
+		{ label: "WR R09", type: "Recommendation", description: "Update Bulletin 118, California's Groundwater Plan", status: "Active", groups: [
+			{label: "WR R09-01", type: "Measure", description: "California Department of Water Resources’ update of Bulletin 118 information (using field data, California Statewide Groundwater Elevation Monitoring Program, and best available science) and identification of the State’s groundwater basins that are in a critical condition of overdraft by December 31, 2014.",
+			status: "Completed", due_date: "12/31/2014"},
+			]},
+		{ label: "WR R10", type: "Recommendation", description: "Implement Groundwater Management Plans in Areas that Recieve Water from the Delta", status: "Active", groups: [
+			{label: "WR R10-01", type: "Measure", description: "Number of water suppliers in areas that receive water from the Sacramento-San Joaquin Delta watershed that have developed groundwater management plans that are consistent with the required and recommended components of groundwater management plans listed in California Department of Water Resources Bulletin 118-03 by 2014.",
+			status: "Active", due_date: "12/31/2014"},
+			]},
+		{ label: "WR R111", type: "Recommendation", description: "Recover and Manage Critically Overdrafted Groundwater Basins", status: "Active", groups: [
+			{label: "WR R11-01", type: "Measure", description: "Identify number of groundwater basins identified by California Department of Water Resources as being in a critical condition of overdraft that have groundwater management plans consistent with the required and recommended components of groundwater management plans listed in the California Department of Water Resources Bulletin 118-03 by 2014.",
+			status: "Active", due_date: "12/31/2014"},
+			{label: "WR R11-02", type: "Measure", description: "SWRCB report to Council on proposed action to address groundwater basins in critical overdraft.",
+			status: "Completed", due_date: "NA"},
+			]},
+		{ label: "WR R12", type: "Recommendation", description: "Complete the Bay Delta Conservation Plan", status: "Active", groups: [
+			{label: "WR R12-01", type: "Measure", description: "The Bay Delta Conservation Plan is completed, and California Department of Water Resources and Bureau of Reclamation have received required take permits by December 31, 2014.",
+			status: "Active", due_date: "12/31/2014"},
+			]},
+		{ label: "WR R13", type: "Recommendation", description: "Complete Surface Water Storage Studies", status: "Completed", groups: [
+			{label: "WR R13-01", type: "Measure", description: "California Department of Water Resources completes surface water storage studies by December 31, 2012, with recommendations for projects to be implemented.",
+			status: "Completed", due_date: "12/31/2012"},
+			]},
+		{ label: "WR R14", type: "Recommendation", description: "Identify Near-term Opportunities for Storage, Use, and Water Transfer Projects", status: "Active", groups: [
+			{label: "WR R14-01", type: "Measure", description: "California Department of Water Resources has completed a survey of past grant applicants to identify projects that may be implemented within the next 5 to 10 years to expand existing surface and groundwater storage facilities, create new storage, improve Delta conveyance facilities, and improve opportunities for water transfers by December 31, 2012.",
+			status: "Completed", due_date: "12/31/2012"},
+			{label: "WR R14-02", type: "Measure", description: "California Water Commission holds hearings and provides recommendation on priority projects by December 31, 2013.",
+			status: "Active", due_date: "12/31/2013"}
+			]},
+		{ label: "WR R15", type: "Recommendation", description: "Improve Water Transfer Procedures", status: "Active", groups: [
+			{label: "WR R15-01", type: "Measure", description: "California Department of Water Resources and the State Water Resources Control Board, in collaboration with the Delta Stewardship Council, have established an advisory group and recommended measures to reduce procedural and administrative impediments to water transfers by December 31, 2016.",
+			status: "Active", due_date: "12/31/2016"},
+			]},
+		{ label: "WR P02", type: "Recommendation", description: "Transparency in Water Contracting", status: "Active", groups: [
+			{label: "WR P2-01", type: "Measure", description: "California Department of Water Resources and Bureau of Reclamation contracting processes have been implemented consistent with applicable policies.",
+			status: "Active", due_date: "NA"},
+			]},
+		{ label: "WR R16", type: "Recommendation", description: "Supplemental Water Use Reporting", status: "Active", groups: [
+			{label: "WR R16-01", type: "Measure", description: "The State Water Resources Control Board has modified its supplemental water diversion and use or progress reports to require additional information on water efficiency, water supply projects, and net (consumptive) use.",
+			status: "Active", due_date: "NA"},
+			]},
+		{ label: "WR R17", type: "Recommendation", description: "Integrated Statewide System for Water Use Reporting", status: "Pending", groups: [
+			{label: "WR R17-01", type: "Measure", description: "California Department of Water Resources has completed the development and initiated implementation of an integrated statewide system for water use reporting in coordination with other State agencies by 2014.",
+			status: "Pending", due_date: "12/31/2014"},
+			]},
+		{ label: "WR R18", type: "Recommendation", description: "California Water Plan", status: "Active", groups: [
+			{label: "WR R18-01", type: "Measure", description: "California Department of Water Resources has modified the California Water Plan update to include specified categories of information to be tracked.",
+			status: "Active", due_date: "NA"},
+			{label: "WR R18-02", type: "Measure", description: "Development of appropriate performance measures will be done by the Delta Stewardship Council in consultation with the agencies. These performance measures will be rolled into the California Water Plan Update.",
+			status: "Active", due_date: "NA"}
+			]},
+		{ label: "WR R19", type: "Recommendation", description: "Financial Needs Assessment", status: "Active", groups: [
+			{label: "WR R19-01", type: "Measure", description: "California Department of Water Resources has prepared an assessment of the State’s water infrastructure.",
+			status: "Active", due_date: "NA"},
+			]}
+	]},
+	{ label: "Water Quality", type: "Chapter", description: "<ul><li>17 Measures: 6 complete, 11 active </li><li> 4 agencies: DSC, SWRCB, DWR, Regional Water Quality Boards </li><li> Linkage to: <ul><li> Water Quality Control Plans </li><li> Central Valley Drinking Water Policy </li><li> Strategic Groundwater Work Plans </li><li> Basin Plans </li></ul></li></ul>"},
+	{ label: "Delta as a Place", type: "Chapter", description: "<ul><li>24 Measures: 2 complete, 20 active, 2 pending </li><li> Agencies: DSC, DPC, DOT, DFW, DWR, ARB, SSJDC, CSP, DBW, CEC, CPUC, and other specific counties, cities, ports</li><li> Linkage to: <ul><li> Local Government Use Plans </li><li> Delta Protection Comission's Land Use and Resource Management Plan </li><li> California State Park's Recreation Proposal </li><li> Delta Protection Commission's Economic Sustainability Plan </li></ul></li></ul>"},
+	{ label: "Risk Reduction", type: "Chapter", description: "<ul><li>19 Measures: 1 complete, 11 active, 7 pending </li><li> 5 agencies: DSC, DWR, CPUC, CVFPB, USACE </li><li> Linkage to: <ul><li> Emergency Action Plans </li><li> Delta Levee Investment Strategy </li></ul></li></ul>"},
+	{ label: "Ecosystem Restoration", type: "Chapter", description: "<ul><li>26 Measures: 1 complete, 20 active, 5 pending </li><li> 7 agencies: SWRCB, DSC, DFW, SSJDC, USACE, BCDC, DWR</li><li> Linkage to: <ul><li> ERP Delta Conservation Strategy </li><li> Suisun Marsh Protection Plan </li><li> Suisun Marsh Preservation Act </li><li> Suisun Marsh Local Protection Program</li></ul></li></ul>"}
+	]}
